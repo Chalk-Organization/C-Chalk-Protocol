6f6c85eee2296713c29749d014870e1bc625bdc3
LemonjamesD <lemonjameslem@protonmail.com>
2022-09-29 22:06:50 -0400

    Implemented `new_vector`

--- src/vector.c
+++ src/vector.c
@@ -1,12 +1,29 @@
 #include "include/vector.h"
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+void
+vector_push (t_vector *self, void *value)
+{
+
+}
 
 t_vector 
 new_vector () 
 {
 	t_vector vector;
-	vector.data = malloc(0);
+	vector.size = sizeof(void *);
+	vector.data = malloc(vector.size);
+	if (vector.data == NULL)
+	{
+		perror("Failed to Allocate `vector`.");
+	}
 	vector.len = 0;
+
+	vector.push = vector_push;
+	vector.get = vector_get;
+	vector.pop = vector_pop;
 
 	return vector;
 }
